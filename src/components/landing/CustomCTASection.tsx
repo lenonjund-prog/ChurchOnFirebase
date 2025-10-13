1b2ba463de89e40dffcc7e79a1973796e83700ee
[dyad] <lenon.jund@gmail.com>
2025-10-13 16:51:56 -0300

    [dyad] wrote 2 file(s)

--- src/components/landing/CustomCTASection.tsx
+++ src/components/landing/CustomCTASection.tsx
@@ -18,7 +18,7 @@ export function CustomCTASection() {
   };
 
   return (
-    <section className="w-full py-12 md:py-24 lg:py-32">
+    <section id="custom-cta" className="w-full py-12 md:py-24 lg:py-32">
       <div className="container px-4 md:px-6">
         <div className="relative overflow-hidden rounded-xl h-[400px] md:h-[500px]">
           <Image
