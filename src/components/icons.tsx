eff6cd087040de70d7595c5450fbfd94808c60c8
[dyad] <lenon.jund@gmail.com>
2025-10-13 18:48:24 -0300

    [dyad] Corrigindo tipos de width e height no componente IgrejaSaaSLogo - wrote 1 file(s)

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -1,10 +1,11 @@
-import React, { type SVGProps } from "react";
+import React from "react";
 import Image from "next/image"; // Importar o componente Image do Next.js
 
 // Ajustado para estender React.ImgHTMLAttributes para compatibilidade com next/image
-interface IgrejaSaaSLogoProps extends React.ImgHTMLAttributes<HTMLImageElement> {
-  // width e height já são parte de ImgHTMLAttributes, não precisam ser redefinidos aqui
-  // className também já é parte de ImgHTMLAttributes
+interface IgrejaSaaSLogoProps extends Omit<React.ImgHTMLAttributes<HTMLImageElement>, 'width' | 'height'> {
+  width?: number; // Especificar que width é um número
+  height?: number; // Especificar que height é um número
+  className?: string; // Manter className como string
 }
 
 export function IgrejaSaaSLogo({ width = 48, height = 48, className, ...props }: IgrejaSaaSLogoProps) {
