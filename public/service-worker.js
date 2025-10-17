ee4b69c02cf51cd0c50d5417dea3e2c9add4513f
[dyad] <lenon.jund@gmail.com>
2025-10-16 21:20:33 -0300

    [dyad] Atualizando manifest e service worker - wrote 2 file(s)

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,54 +1,47 @@
-const CACHE_NAME = 'churchon-cache-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/globals.css',
-  '/logo.png',
-  '/icon.png',
-  // Adicione outros recursos estáticos importantes aqui
-  // Ex: '/_next/static/css/main.css', '/_next/static/chunks/main.js'
-  // Para Next.js, os caminhos exatos de build podem variar, então é melhor
-  // deixar o Service Worker mais genérico ou usar uma biblioteca PWA para Next.js.
-  // Para este exemplo, focaremos nos assets que você já tem na pasta public.
-];
+// This is the "Offline page" service worker
 
-self.addEventListener('install', (event) => {
-  console.log('[Service Worker] Installing...');
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+
+const CACHE = "pwabuilder-page";
+
+// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
+const offlineFallbackPage = "ToDo-replace-this-name.html";
+
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
+});
+
+self.addEventListener('install', async (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => {
-        console.log('[Service Worker] Caching app shell');
-        return cache.addAll(urlsToCache);
-      })
+    caches.open(CACHE)
+      .then((cache) => cache.add(offlineFallbackPage))
   );
 });
 
-self.addEventListener('activate', (event) => {
-  console.log('[Service Worker] Activating...');
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheName !== CACHE_NAME) {
-            console.log('[Service Worker] Deleting old cache:', cacheName);
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
-});
+if (workbox.navigationPreload.isSupported()) {
+  workbox.navigationPreload.enable();
+}
 
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request)
-      .then((response) => {
-        // Cache hit - return response
-        if (response) {
-          return response;
+  if (event.request.mode === 'navigate') {
+    event.respondWith((async () => {
+      try {
+        const preloadResp = await event.preloadResponse;
+
+        if (preloadResp) {
+          return preloadResp;
         }
-        // No cache hit - fetch from network
-        return fetch(event.request);
-      })
-  );
+
+        const networkResp = await fetch(event.request);
+        return networkResp;
+      } catch (error) {
+
+        const cache = await caches.open(CACHE);
+        const cachedResp = await cache.match(offlineFallbackPage);
+        return cachedResp;
+      }
+    })());
+  }
 });
